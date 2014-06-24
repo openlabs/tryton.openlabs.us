c1e2f0ff1097c220e0a10eaddb00625136368664
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-06-24 10:01:07 +0530

    Add article and index page to theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,6 +30,9 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+# Directory where content is located
+PATH = 'content'
+
 # Set the theme folder
 THEME = 'theme/'
 
